159e2d4d6074ca5b9ad64331c619df46b4261a21
Mitesh Khare <miteshkhare.17@gmail.com>
2016-09-07 18:41:48 +0530

    Bootstrap Removed & canvas initialized

---
+++ js/gameMechanincs.js
@@ -0,0 +1,12 @@
+
+
+var canvas = document.getElementById('gameCanvas');
+
+// Obtain a graphics context on the canvas element for drawing.
+var context = canvas.getContext('2d');
+
+//resize canvas to device size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
