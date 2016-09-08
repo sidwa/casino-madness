f5797fdddc947ef55e6bc41b5360f937e97a0dc5
Mitesh Khare <miteshkhare.17@gmail.com>
2016-09-08 20:37:49 +0530

    Update

--- public/gameMechanics.js
+++ public/gameMechanics.js
@@ -6,17 +6,16 @@ var height = window.innerHeight;
 var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.spritesheet('wheel', 'assets/wheel.png', 600, 600, 10);
+    game.load.spritesheet('wheel', 'assets/wheel.png', 600, 600, 30);
 }
 
 function create() {
     var wheel = game.add.sprite(0, 0, 'wheel');
-    
-    var rotate = wheel.animations.add('rotate');
-    
-    wheel.animations.play('rotate', 30, true);
+    wheel.animations.add('rotate');
+    wheel.animations.play('rotate',30,true);
 }
 
 function update() {
+    
 }
 
