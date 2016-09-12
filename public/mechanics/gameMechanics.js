41ac9fc53aafb6cea3b92951a3d19e064b432b68
Mitesh Khare <miteshkhare.17@gmail.com>
2016-09-12 17:09:37 +0530

    update

--- public/mechanics/gameMechanics.js
+++ public/mechanics/gameMechanics.js
@@ -14,35 +14,39 @@ function preload() {
 }
 
 function create() {
-    playWheelAnimation();
+    ROULLET.playWheelAnimation();
 }
 
 function update() {
-    
+
 }
 
-function playWheelAnimation(){
-    var wheel_rotate = game.add.sprite(0, 0, 'wheel_rotate');
-    wheel_rotate.scale.setTo(1,1);
-    wheel_rotate.animations.add('rotate');
-    
-    var wheel_initiate = game.add.sprite(0, 0, 'wheel_initiate');
-    var count = 0;
-    
-    wheel_initiate.scale.setTo(1,1);
-    wheel_initiate.animations.add('rotate_begin');
-    
-    wheel_initiate.animations.play('rotate_begin',15,false);
-    wheel_initiate.events.onAnimationComplete.add(function(){
-        wheel_initiate.destroy();
-        
-        wheel_rotate.animations.play('rotate',15,true);
-        wheel_rotate.events.onAnimationLoop.add(function(){
-           if(count >= 2){
-                wheel_rotate.animations.stop();
-                //wheel_rotate.destroy();
-           }
-           count++;
+var ROULLET ={
+    id : "mit17k",
+    cashbalance : 25000,
+    playWheelAnimation : function (){
+        var wheel_rotate = game.add.sprite(0, 0, 'wheel_rotate');
+        wheel_rotate.scale.setTo(1,1);
+        wheel_rotate.animations.add('rotate');
+
+        var wheel_initiate = game.add.sprite(0, 0, 'wheel_initiate');
+        var count = 0;
+
+        wheel_initiate.scale.setTo(1,1);
+        wheel_initiate.animations.add('rotate_begin');
+
+        wheel_initiate.animations.play('rotate_begin',15,false);
+        wheel_initiate.events.onAnimationComplete.add(function(){
+            wheel_initiate.destroy();
+
+            wheel_rotate.animations.play('rotate',15,true);
+            wheel_rotate.events.onAnimationLoop.add(function(){
+               if(count >= 2){
+                    wheel_rotate.animations.stop();
+                    //wheel_rotate.destroy();
+               }
+               count++;
+            });
         });
-    });
-}
+    }
+};
