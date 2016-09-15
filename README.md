f103fc543193f23db806ca4467e2f641efc198f7
JacquesFernandes <jacques.fernandes.1337@gmail.com>
2016-09-16 00:15:24 +0530

    minor changes to Readme and other stuff

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Virtual Casino :: Jacques' branch
 
 #JacquesFernandes
 Sup! "Lawrenz" here!
+
+PYTHON SERVER USAGE:
+ - python3 /python/server.py <port> [--verbose]
+  - here "/" is the root dir of the site... (i.e. where this file (README.md) is...)
--- python/server.py
+++ python/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import http.server;
 import sys;
 from custom_handler import CustomHandler;
@@ -8,7 +9,7 @@ super_print=print; #assigning orignal print function to "super print", so I can 
 
 ''' CLASSES '''
 class ArgParser:
-	
+
 	def __init__(self,args):
 		self.origin = args;
 		self.flags = list();
@@ -16,27 +17,27 @@ class ArgParser:
 		self.__set_flags();
 		self.__set_data();
 		return;
-		
+
 	def __set_flags(self):
 		for part in self.origin:
 			if "-" in part or "--" in part:
 				self.flags.append(part.strip("-"));
 				self.origin.remove(part);
 		return;
-	
+
 	def __set_data(self):
 		for part in self.origin:
 			if "-" not in part or "--" not in part:
 				self.data.append(part);
 				self.origin.remove(part);
 		return;
-	
+
 	def __str__(self):
 		ret = str();
 		ret += "DATA : "+str(self.data)+"\n";
 		ret += "FLAGS: "+str(self.flags);
 		return(ret);
-		
+
 
 
 ''' FUNCTIONS '''
@@ -51,12 +52,12 @@ def print(message):
 if __name__ == "__main__":
 	port = int();
 	address = "";
-	
+
 	args = ArgParser(sys.argv[1:]);
-	
+
 	if "verbose" in args.flags:
 		VERBOSE = True;
-	
+
 	if len(args.data) < 1:
 		print("No arguments were passed! Using Default Port");
 		port = DEFAULT_PORT;
@@ -67,12 +68,12 @@ if __name__ == "__main__":
 		except ValueError:
 			error_message("Incompatible data for port number, please make sure that first argument is a number");
 			exit();
-	
+
 	combo = (address,port);
 	hs = http.server;
 	handler = CustomHandler;
 	http_daemon = hs.HTTPServer(combo,handler);
-	
+
 	try:
 		print("Server has started...");
 		http_daemon.serve_forever();
